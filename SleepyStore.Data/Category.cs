60d5bdef12e8ed9d10382938d5a1fba3959fa5f0
JoshHambright <josh.hambright@gmail.com>
2020-11-17 11:01:06 -0500

    uncomment virtual and forgeign key stuff

--- SleepyStore.Data/Category.cs
+++ SleepyStore.Data/Category.cs
@@ -14,7 +14,6 @@ namespace SleepyStore.Data
         public string CategoryName { get; set; }
         public DateTime CreatedUtc { get; set; }
         public DateTime? UpdatedUtc { get; set; }
-        //Uncomment below to link Item and Category Class
-        //public virtual List<Item> Items { get; set; } = new List<Item>();
+        public virtual List<Item> Items { get; set; } = new List<Item>();
     }
 }
