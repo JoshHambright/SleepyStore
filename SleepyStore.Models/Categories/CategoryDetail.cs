07d7a34314ae6c82a7f3452ea6a654a147c46ab8
JoshHambright <josh.hambright@gmail.com>
2020-11-16 19:51:08 -0500

    Category Data Models

---
+++ SleepyStore.Models/Categories/CategoryDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleepyStore.Models.Categories
+{
+    public class CategoryDetail
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        //Uncomment below to link Item and Category Class
+        //public virtual List<Item> Items { get; set; } = new List<Item>();
+    }
+}
---
+++ SleepyStore.Models/Categories/CategoryList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleepyStore.Models.Categories
+{
+    public class CategoryList
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        
+}
