075eadbc651f6103e35d3f97ececbea09358d06c
JoshHambright <josh.hambright@gmail.com>
2020-11-16 19:31:14 -0500

    Item class

---
+++ SleepyStore.Data/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleepyStore.Data
+{
+    public class Item
+    {
+        [Key]
+        public int ItemId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public int Inventory { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+
+        //public double Rating { get; set; }
+        //[ForeignKey(nameof(Category))]
+        public int CategoryID { get; set; }
+        //public virtual Category Category { get; set; }
+    }
+}
